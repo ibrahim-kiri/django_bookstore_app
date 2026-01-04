6ece58f15ec65d55b1567d21ff5889b06ee56f01
ibrahim-kiri <ibrahimkiri@gmail.com>
2026-01-04 07:59:17 +0300

    added email configurations

--- django_project/settings.py
+++ django_project/settings.py
@@ -150,4 +150,5 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = "admin@djangobookstore.com"
